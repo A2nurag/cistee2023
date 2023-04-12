1dddf8697da128319c4645024119f2898973e702
a2nurag <anurag2sarkar@gmail.com>
2023-04-12 22:49:35 +0530

    description

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Footer from '../components/footer'
+import { Html, Head, Main, NextScript } from "next/document";
+import Footer from "../components/footer";
 
 export default function Document() {
   return (
-    <Html lang='en'>
-      <Head />
+    <Html lang="en">
+      <Head>
+        <meta
+          name="description"
+          content="CISTEE 2023 is being organized by Department of Electrical Engineering, National Institute of Technology Durgapur, in collaboration with MANIT Bhopal during December 16-17, 2023."
+        />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      </Head>
       <body>
         <Main />
         <NextScript />
         <Footer />
       </body>
     </Html>
-  )
+  );
 }
