39365568381cdd40eaa3485c1b2ef22be0228bad
A2nurag <anurag2sarkar@gmail.com>
2023-11-28 17:14:33 +0530

    updates nov 28

--- pages/registration.js
+++ pages/registration.js
@@ -24,16 +24,17 @@ export default function Registration() {
                 Registration Fee:
               </h2>
               <p className="my-3 md:ml-6">
-                One of the authors of each paper must have full registration and present the paper in person at the Conference. Please
-                note that the fees as shown below are inclusive of all taxes and levies. Any gateway charges along with applicable taxes or levies, if any, will have
-                to be borne by the registrant.
+                One of the authors of each paper must have full registration and
+                present the paper in person at the Conference. Please note that
+                the fees as shown below are inclusive of all taxes and levies.
+                Any gateway charges along with applicable taxes or levies, if
+                any, will have to be borne by the registrant.
               </p>
 
               <img
                 src="/reg%20fee.png"
                 className="my-4 w-[100%] mx-auto lg:w-[80%]"
                 alt="Registration fees"
-          
               />
             </div>
             {/* Steps for Registration: */}
@@ -82,16 +83,6 @@ export default function Registration() {
                   cistee2023@gmail.com
                 </a>
               </p>
-              <p className="my-2 md:ml-6">
-                <span className="font-semibold">Step 4:</span> <br />
-                Then fill up the{" "}
-                <a
-                  href="https://docs.google.com/forms/d/1NZdKKS-VzqsIwP9kyJrXMCPAzYZf4y6HlXdlQ_OfTeM"
-                  className="font-semibold text-blue-800 hover:underline"
-                >
-                  Registration Form
-                </a>
-              </p>
             </div>
             <div className="my-6">
               <h2 className="my-2 text-2xl font-bold text-slate-700">
@@ -105,21 +96,23 @@ export default function Registration() {
                       Name of Account
                     </td>
                     <td className="border-2 border-zinc-700 p-2 pr-16 bg-blue-100">
-                    CEP NIT Durgapur
+                      CEP NIT Durgapur
                     </td>
                   </tr>
                   <tr>
                     <td className="border-2 border-zinc-700 p-2 font-bold">
                       Account Number
                     </td>
-                    <td className="border-2 border-zinc-700 p-2">37850318679</td>
+                    <td className="border-2 border-zinc-700 p-2">
+                      37850318679
+                    </td>
                   </tr>
                   <tr>
                     <td className="border-2 border-zinc-700 p-2 font-bold bg-blue-100">
                       Name of Bank
                     </td>
                     <td className="border-2 border-zinc-700 p-2 bg-blue-100">
-                    State Bank of India
+                      State Bank of India
                     </td>
                   </tr>
                   <tr>
@@ -127,7 +120,7 @@ export default function Registration() {
                       Branch Name
                     </td>
                     <td className="border-2 border-zinc-700 p-2">
-                    R.E College (Durgapur)
+                      R.E College (Durgapur)
                     </td>
                   </tr>
                   <tr>
@@ -135,7 +128,8 @@ export default function Registration() {
                       Bank Address
                     </td>
                     <td className="border-2 border-zinc-700 p-2 bg-blue-100">
-                    SBI, RE College, M. G. Avenue, Durgapur-713209, West Bengal, India
+                      SBI, RE College, M. G. Avenue, Durgapur-713209, West
+                      Bengal, India
                     </td>
                   </tr>
                   <tr>
@@ -143,7 +137,7 @@ export default function Registration() {
                       IFSC Code
                     </td>
                     <td className="border-2 border-zinc-700 p-2">
-                    SBIN0002108
+                      SBIN0002108
                     </td>
                   </tr>
                   <tr>
@@ -151,7 +145,7 @@ export default function Registration() {
                       CIF Number
                     </td>
                     <td className="border-2 border-zinc-700 p-2 bg-blue-100">
-                    81244040204
+                      81244040204
                     </td>
                   </tr>
                   <tr>
@@ -159,16 +153,14 @@ export default function Registration() {
                       MICR Code
                     </td>
                     <td className="border-2 border-zinc-700 p-2 bg-blue-100">
-                    713002204
+                      713002204
                     </td>
                   </tr>
                   <tr>
                     <td className="border-2 border-zinc-700 p-2 font-bold">
                       SWIFT Code
                     </td>
-                    <td className="border-2 border-zinc-700 p-2">
-                    SBININBB
-                    </td>
+                    <td className="border-2 border-zinc-700 p-2">SBININBB</td>
                   </tr>
                 </tbody>
               </table>
@@ -200,14 +192,19 @@ export default function Registration() {
               </h2>
 
               <p className="mt-2 md:ml-6">
-                1. The page limit for each accepted paper is 12 pages. If needed, up to TWO (03) additional pages can be added (apart from basic 12 pages)
-                by paying an extra charge (Indian Delegates: INR 500 / Foreign Delegates: $ 15) per additional page at the time of author registration
-                payment to make the final version of the paper.
+                1. The page limit for each accepted paper is 12 pages. If
+                needed, up to TWO (03) additional pages can be added (apart from
+                basic 12 pages) by paying an extra charge (Indian Delegates: INR
+                500 / Foreign Delegates: $ 15) per additional page at the time
+                of author registration payment to make the final version of the
+                paper.
               </p>
 
               <p className="mt-2 md:ml-6">
-                2. At least one of the authors of every accepted paper must register for the conference as author and present the paper in order for it to be
-                included in the conference proceedings of CISTEE 2023.
+                2. At least one of the authors of every accepted paper must
+                register for the conference as author and present the paper in
+                order for it to be included in the conference proceedings of
+                CISTEE 2023.
               </p>
 
               <p className="mt-2 md:ml-6">
@@ -215,30 +212,36 @@ export default function Registration() {
               </p>
 
               <p className="mt-2 md:ml-6">
-                4. If more than one author of an accepted paper wishes to attend the conference, all such authors need to be registered separately by paying
-                applicable registration fee.
+                4. If more than one author of an accepted paper wishes to attend
+                the conference, all such authors need to be registered
+                separately by paying applicable registration fee.
               </p>
 
               <p className="mt-2 md:ml-6">
-                5. Additional authors (other than those who are included in the manuscript submitted for review) cannot be added to the camera-ready
-                paper.
+                5. Additional authors (other than those who are included in the
+                manuscript submitted for review) cannot be added to the
+                camera-ready paper.
               </p>
 
               <p className="mt-2 md:ml-6">
-                6. <b>Non-refundable registration fee</b> must be paid prior to uploading Camera-ready version of the paper and copyright form.
+                6. <b>Non-refundable registration fee</b> must be paid prior to
+                uploading Camera-ready version of the paper and copyright form.
               </p>
 
               <p className="mt-2 md:ml-6">
-                7. No accepted paper is included in the proceedings without the payment of the required registration fee.
+                7. No accepted paper is included in the proceedings without the
+                payment of the required registration fee.
               </p>
 
               <p className="mt-2 md:ml-6">
-                8. No accepted paper is included in the proceedings without being presented at the conference.
+                8. No accepted paper is included in the proceedings without
+                being presented at the conference.
               </p>
 
               <p className="mt-2 md:ml-6">
-                9. At least one of the authors of every accepted paper must register for the conference as author and present the paper in order to get
-                presentation certificate.
+                9. At least one of the authors of every accepted paper must
+                register for the conference as author and present the paper in
+                order to get presentation certificate.
               </p>
             </div>
             <div className="my-6">
@@ -247,26 +250,35 @@ export default function Registration() {
               </h3>
 
               <p className="mt-2 md:ml-6">
-                1. All participants need to be registered to participate in the CISTEE 2023 by paying the requisite registration fees.
+                1. All participants need to be registered to participate in the
+                CISTEE 2023 by paying the requisite registration fees.
               </p>
 
               <p className="mt-2 md:ml-6">
-                2. Students (UG, PG, Research scholars) are requested to upload valid student ID card (front and back side) during the online registration
-                process, if <b>registering under the student category</b>.
+                2. Students (UG, PG, Research scholars) are requested to upload
+                valid student ID card (front and back side) during the online
+                registration process, if{" "}
+                <b>registering under the student category</b>.
               </p>
               <p className="mt-2 md:ml-6">
-                3. Please keep a copy/receipt of the transaction ID, generated by the payment gateway as well as by your bank. You may need these later if
-                you need to track your payment/registration.
+                3. Please keep a copy/receipt of the transaction ID, generated
+                by the payment gateway as well as by your bank. You may need
+                these later if you need to track your payment/registration.
               </p>
               <p className="mt-2 md:ml-6">
-                4. All correspondence regarding registration for CISTEE 2023 should contain Paper ID as well as registration confirmation number.
+                4. All correspondence regarding registration for CISTEE 2023
+                should contain Paper ID as well as registration confirmation
+                number.
               </p>
               <p className="mt-2 md:ml-6">
-                5. The Organizer of CISTEE 20223, or the Organizing Institute (NIT Durgapur), will not be responsible for any loss, financial or otherwise,
-                caused by improper transactions conducted online. While organizers have taken all reasonable precautions to ensure a safe online
-                transaction, it is the registrant&apos;s responsibility, while paying the registration fee, to secure their user IDs, passwords, Paper ID, etc.
+                5. The Organizer of CISTEE 20223, or the Organizing Institute
+                (NIT Durgapur), will not be responsible for any loss, financial
+                or otherwise, caused by improper transactions conducted online.
+                While organizers have taken all reasonable precautions to ensure
+                a safe online transaction, it is the registrant&apos;s
+                responsibility, while paying the registration fee, to secure
+                their user IDs, passwords, Paper ID, etc.
               </p>
-
             </div>
             <div className="my-6">
               <h3 className="my-2 text-2xl font-bold text-slate-700">
@@ -276,7 +288,8 @@ export default function Registration() {
               <p className="mt-2 md:ml-6">
                 Any accepted paper included in the final program is expected to
                 have at least one author attend and present the paper at the
-                conference. Non-presented paper will not be submitted to Springer as per Springer no-show policy.
+                conference. Non-presented paper will not be submitted to
+                Springer as per Springer no-show policy.
               </p>
             </div>
           </div>
